289dfb6f05f3c508255ac2995e5ce8aac5d6147d
Bryce W. Frazier <brycewill.id@gmail.com>
2025-02-22 20:40:44 -0900

    Added tank senesor, cleaned up code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // main.rs (rustc 1.79.0-nightly)
 // Bryce W. Frazier
 // Started: 2025-02-21
-// Updated: 2025-02-21
+// Updated: 2025-02-22
 //
 // Main file for CS241 project, automatic plant wating system
 #![no_std]
@@ -9,6 +9,7 @@
 
 use panic_halt as _;
 use arduino_hal::prelude::_unwrap_infallible_UnwrapInfallible;
+use ufmt::uwriteln;
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -22,20 +23,30 @@ fn main() -> ! {
     let mut error_led = pins.d2.into_output();
     let soil_prob = pins.a0.into_analog_input(&mut adc);
     let overflow_detector = pins.d3.into_pull_up_input();
+    let tank_low = pins.d4.into_pull_up_input();
     
     //Init serial
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     loop {
         let input = soil_prob.analog_read(&mut adc);
-        ufmt::uwriteln!(&mut serial, "{}", input).unwrap_infallible();
+        uwriteln!(&mut serial, "{}", input)
+            .unwrap_infallible();
 
         //Stop if plant pot is overflowing
         while overflow_detector.is_low() {
             pump.set_low(); // to insure pump is off
-            error_led.set_high(); // Visual indcator TODO use for empty tank warning NOT OVERFLOW
-            ufmt::uwriteln!(&mut serial, "Error:\tContainer Overflow").unwrap_infallible();
-            arduino_hal::delay_ms(10);
+            uwriteln!(&mut serial, "Error:\tContainer Overflow")
+                .unwrap_infallible();
+            arduino_hal::delay_ms(100);
+        }
+
+        while tank_low.is_high() {
+            pump.set_low(); // to insure pump is off
+            error_led.set_high();
+            uwriteln!(&mut serial, "Error:\tTank is empty")
+                .unwrap_infallible();
+            arduino_hal::delay_ms(100);
             error_led.set_low();
         }
 
